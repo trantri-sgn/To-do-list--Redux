2a4942dff5f3fbcade0040937ae73122a2fd016e
unknown <tminhtri93@gmail.com>
2020-06-06 01:14:30 +0700

    Updates

---
+++ precache-manifest.ba17c68f21a79b4283c931bf3f5ab8f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edf17c7a42b2f04cfe9dc6ceaa9c382a",
+    "url": "/To-do-list--Redux/index.html"
+  },
+  {
+    "revision": "4567604958a885c04105",
+    "url": "/To-do-list--Redux/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7466bee5607ad1b2ffbf",
+    "url": "/To-do-list--Redux/static/css/main.c9b1ecb6.chunk.css"
+  },
+  {
+    "revision": "4567604958a885c04105",
+    "url": "/To-do-list--Redux/static/js/2.7b78f05a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/To-do-list--Redux/static/js/2.7b78f05a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7466bee5607ad1b2ffbf",
+    "url": "/To-do-list--Redux/static/js/main.a157b992.chunk.js"
+  },
+  {
+    "revision": "d90df7605563d272c4e5",
+    "url": "/To-do-list--Redux/static/js/runtime-main.24f51748.js"
+  }
+]);
